50a4e463eb275617eba40ab8b8fc91697fc277fc
Francisco Batista <francisco.batista@wizeline.com>
2022-05-10 19:24:06 -0500

    feat: project created

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Hihi</h1>
+      <p>
+        My name is Paco, this should be my resume, but I'm still building it, please be patient...
+      </p>
+    </div>
+  )
+}
+
+export default App
