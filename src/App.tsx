3153e4590b1b024564518a97e1a63b7c785103a1
Francisco Batista <fbatista.fxbi@gmail.com>
2022-05-10 20:31:52 -0500

    test: testing CNAME

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <h1>Hihi</h1>
       <p>
-        My name is Paco, this should be my resume, but I'm still building it, please be patient...
+        My name is Paco, this should be my resume, but I'm still building it, please be patient....
       </p>
       <p>
         To see the source code, click <a href="https://github.com/pacobatistadev/react.resume">here</a>
