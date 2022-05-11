00dec52326ac9c1d9f4b40514a9ecab8f0c7025c
Francisco Batista <fbatista.fxbi@gmail.com>
2022-05-10 20:19:40 -0500

    feat: added reference links

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # react.resume
-My resume
+My resume hosted on [me.pacobatista.dev](https://me.pacobatista.dev)
--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <h1>Hihi</h1>
       <p>
         My name is Paco, this should be my resume, but I'm still building it, please be patient...
+      </p>
+      <p>
+        To see the source code, click <a href="https://github.com/pacobatistadev/react.resume">here</a>
       </p>
     </div>
   )
